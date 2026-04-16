d7e82703f47276384a7e54663e7682ced3b2e466
t-hino <t-hino@CD26309L.arksystems.co.jp>
2026-04-16 14:29:53 +0900

    Q７-１~７-３まで完了

--- java_comprehension_exercises_volume1/src/advanced/question07/Question07_1.java
+++ java_comprehension_exercises_volume1/src/advanced/question07/Question07_1.java
@@ -1,12 +1,38 @@
 package advanced.question07;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Question07_1 {
 
 	public static void main(String[] args) throws IOException {
 
 		// TODO:ここに実装
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		String[] itemNames = new String[5];
+		int[] prices = new int[5];
+
+		// 1. 商品名の入力
+		for (int i = 0; i < itemNames.length; i++) {
+			System.out.print("商品 " + (i + 1) + " を入力してください: > ");
+			itemNames[i] = reader.readLine();
+		}
+
+		// 2. 単価の入力
+		System.out.println("各商品の単価を順に入力してください:");
+		for (int i = 0; i < prices.length; i++) {
+			System.out.print(itemNames[i] + "の単価: > ");
+			prices[i] = Integer.parseInt(reader.readLine());
+		}
+
+		// 3. 登録商品一覧の出力
+		System.out.println("登録商品一覧");
+		for (int i = 0; i < itemNames.length; i++) {
+			System.out.println(itemNames[i] + "：" + prices[i] + " 円");
+		}
 
 	}
 
--- java_comprehension_exercises_volume1/src/advanced/question07/Question07_3.java
+++ java_comprehension_exercises_volume1/src/advanced/question07/Question07_3.java
@@ -1,6 +1,8 @@
 package advanced.question07;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Question07_3 {
 
@@ -9,12 +11,46 @@ public class Question07_3 {
 		// 登録済み商品（値は変更可）
 		String[] itemNames = { "ノート", "ボールペン", "消しゴム", "はさみ", "のり" };
 		// 単価（値は変更可）
-		int[] prices = { 100, 200, 300, 400, 500 };
+		int[] prices = { 120, 80, 50, 250, 100 };
 		// 購入数（変更不可）
 		int[] amounts = new int[5];
 
 		// TODO: 実装ここから
 
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		//在庫数
+		int[] stocks = new int[5];
+		for (int i = 0; i < itemNames.length; i++) {
+			System.out.print(itemNames[i] + "の在庫数: > ");
+			stocks[i] = Integer.parseInt(reader.readLine());
+		}
+
+		// 2. 購入数の入力（在庫チェック付き）
+
+		for (int i = 0; i < itemNames.length; i++) {
+			while (true) {
+				System.out.print(itemNames[i] + "の購入数: > ");
+				int inputAmount = Integer.parseInt(reader.readLine());
+
+				if (inputAmount <= stocks[i]) {
+					amounts[i] = inputAmount;
+					break; // 在庫内であればループを抜ける
+				} else {
+					System.out.println("在庫を超えています。もう一度入力してください。");
+				}
+			}
+		}
+
+		// 3. 購入数、在庫数、合計金額の表示
+		int totalSum = 0;
+		for (int i = 0; i < itemNames.length; i++) {
+			System.out.println(itemNames[i] + "：購入 " + amounts[i] + " 個");
+			System.out.println("在庫 " + stocks[i] + " 個");
+			totalSum += prices[i] * amounts[i];
+		}
+
+		System.out.println("合計金額：" + totalSum + " 円");
 
 	}
 }
